5b19f6826762fc7171339ea90e6e0ca5e42f6258
Andy Mancuso <andymancuso5@gmail.com>
2022-12-15 19:52:51 -0300

    Creación de landing y crear todo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
+import { useState } from "react";
 
 
 const App = () => {
+
+  const [todos, setTodos] = useState ([
+    "Hola, soy un todo",
+    "El bicho"
+  ])
+  
+  const [todo, setTodo] = useState ("")
+
+  /**
+   * Esta función esta ligada al input 
+   * y se ejecuta al ingresar o borrar un caractér
+   */
+  const handleChange = (e) => {
+    const newText = e.target.value
+    setTodo (newText)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const list = [...todos, todo]
+    setTodos(list)
+    setTodo("")
+  }
+
+
   return (
     <div>
-      Todos Mancussi
+
+      <h1>
+        Todo's Mancussi
+      </h1>
+
+      <form onSubmit={handleSubmit}>
+      <input 
+       value= {todo}
+       onChange={handleChange}
+       placeholder="Ingresar todo..." />
+
+      <button>Publicar</button>
+      </form>
+
+      {todos.map(item => {
+        return <p key={item}>- {item}</p>
+      })}
+      
     </div>
   );
 }
